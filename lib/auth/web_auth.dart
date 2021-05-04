9066a853e3d9ae17b0eb936c20990eafc2caba58
AKPAKPAN ANIEKAN-ABASI <akpakpan2535@bazeuniversity.edu.ng>
2021-05-04 22:29:25 +0100

    commit

--- lib/auth/web_auth.dart
+++ lib/auth/web_auth.dart
@@ -1,7 +1,6 @@
 import 'package:chat_app/auth/auth_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class _WebAuth implements AuthProviderBase {
   @override
